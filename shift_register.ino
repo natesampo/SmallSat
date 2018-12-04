bf6e9d1682de69bb1814468025983a0c9b431f84
guokevin <kevin.guo@students.olin.edu>
2018-12-04 16:38:18 -0500

    Update to include delay and automatic shifting

--- shift_register.ino
+++ shift_register.ino
@@ -1,8 +1,8 @@
-int SER_Pin = 2;
-int RCLK_Pin = 3;
-int SRCLK_Pin = 4;
-int SRCLR_Pin = 5;
-int button_Pin = 6;
+int SER_Pin = 2;      //Input for next pin to be shifted in
+int RCLK_Pin = 3;     //Needs to be pulled HIGH to set the output to the new shift register values, This must be pulled HIGH directly after SRCLK has gone LOW again.
+int SRCLK_Pin = 4;    //When this pin is pulled HIGH, it will shift the register.
+int SRCLR_Pin = 5;    //Will empty the whole Shift Register if pulled LOW, must be pulled HIGH to enable.
+int button_Pin = 6;   //Button input for shifting the register
 
 boolean started = false;
 
@@ -30,24 +30,31 @@ void loop() {
   if(val == HIGH){
     //If there have been shifts, then shift everything over by inputting LOW
     if(started){
-      digitalWrite(SRCLK_Pin, HIGH);
-      delay(300);
-      digitalWrite(SRCLK_Pin, LOW);
-      digitalWrite(RCLK_Pin, HIGH);
-      delay(300);
       digitalWrite(RCLK_Pin, LOW);
+      digitalWrite(SRCLK_Pin, LOW);
+      digitalWrite(SRCLK_Pin, HIGH);
+      digitalWrite(RCLK_Pin, HIGH);
       Serial.println("started");
+      delay(1200);
+      digitalWrite(RCLK_Pin, LOW);
+      digitalWrite(SRCLK_Pin, LOW);
+      digitalWrite(SRCLK_Pin, HIGH);
+      digitalWrite(RCLK_Pin, HIGH);
     }
     //Makes first input to shift register HIGH if there has not been any shifts yet
     else{
       started = true;
+      digitalWrite(RCLK_Pin, LOW);    //Set LOW Before setting new Register Values
+      digitalWrite(SRCLK_Pin, LOW);   //Set LOW before shifting register
+      digitalWrite(SRCLK_Pin, HIGH);  //Shift Register
+      digitalWrite(RCLK_Pin, HIGH);   //Set New Register Values
+      digitalWrite(SER_Pin, LOW);     //Sets future pin inputs to LOW
+      Serial.println("not started");
+      delay(1200);                    //Shift Register after [t] time, where t is burn wire on time
       digitalWrite(RCLK_Pin, LOW);
       digitalWrite(SRCLK_Pin, LOW);
-      delay(300);
       digitalWrite(SRCLK_Pin, HIGH);
       digitalWrite(RCLK_Pin, HIGH);
-      digitalWrite(SER_Pin, LOW);
-      Serial.println("not started");
     }
   }
 }
